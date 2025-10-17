be0888bf9f1dde99c171c223b399528ac62791a4
SarahDebruyne <sarahdebruyne5@gmail.com>
2025-10-17 09:12:35 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Mentions from "./pages/Contact";
+import Portfolio from "./pages/portfolio";
+import Services from "./pages/services";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <nav>
+            <link href="/">Accueil</link>
+            <link href="/portfolio">Portfolio</link>
+            <link href="/services">Services</link>
+            <link href="/contact">Contact</link>
+            <Routes>
+              <Route path="/" element={<Accueil />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/mentions" element={<MentionsLegales />} />
+        </Routes>
+          </nav>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // collapse, modal, accordion
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
